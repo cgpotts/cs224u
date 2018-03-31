ece174f7d61820c68abe3abdd809dff02654c661
Christopher Potts <cgpotts@gmail.com>
2018-03-31 12:44:46 -0700

    Tests for models, vsm.py, utils.py, and retrofitting.py

---
+++ test/test_retrofitting.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+import pytest
+import retrofitting
+from retrofitting import Retrofitter
+
+
+@pytest.fixture
+def retrofitter():
+    return Retrofitter(
+        max_iter=100,
+        alpha=None,
+        beta=None,
+        tol=1e-2,
+        verbose=False,
+        introspecting=False)
+
+
+def test_identical_vectors(retrofitter):
+    X = pd.DataFrame([
+        [0.1, 0.3],
+        [0.1, 0.3]])
+    edges = {0: {1}, 1: {0}}
+    Y = retrofitter.fit(X, edges)
+    assert X.loc[0].equals(Y.loc[0])
+
+
+def test_average_with_neighbor(retrofitter):
+    X = pd.DataFrame([
+        [0.5, 0.0],
+        [0.0, 0.5]])
+    edges = {0: {1}, 1:set()}
+    Y = retrofitter.fit(X, edges)
+    assert np.array_equal(Y.loc[0], np.array([0.25, 0.25]))
+    assert X.loc[1].equals(Y.loc[1])
+
+
+def test_mutual_averaging(retrofitter):
+    retrofitter.tol = 1e-10
+    X = pd.DataFrame([
+        [0.5, 0.0],
+        [0.0, 0.5]])
+    edges = {0: {1}, 1: {0}}
+    Y = retrofitter.fit(X, edges)
+    Y = Y.round(6)
+    assert np.array_equal(Y.loc[0], np.array([0.333333, 0.166667]))
+    assert np.array_equal(Y.loc[1], np.array([0.166667, 0.333333]))
+
+
+def test_alpha_setting(retrofitter):
+    retrofitter.alpha = lambda x: 0.0
+    X = pd.DataFrame([
+        [0.5, 0.0],
+        [0.0, 0.5]])
+    edges = {0: {1}, 1:set()}
+    Y = retrofitter.fit(X, edges)
+    assert np.array_equal(Y.loc[0], np.array([0.0, 0.5]))
+
+
+def test_plot_retro_path():
+    Q_hat = pd.DataFrame([
+        [0.0, 0.0],
+        [0.0, 0.5],
+        [0.5, 0.0]],
+        columns=['x', 'y'])
+    edges = {0: {1, 2}, 1: set(), 2: set()}
+    retrofitting.plot_retro_path(Q_hat, edges)
---
+++ test/test_vsm.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pandas as pd
+import pytest
+import vsm
+
+
+@pytest.fixture
+def df():
+    vocab = ['ab', 'bc', 'cd', 'de']
+    df = pd.DataFrame([
+        [  4.,   4.,   2.,   0.],
+        [  4.,  61.,   8.,  18.],
+        [  2.,   8.,  10.,   0.],
+        [  0.,  18.,   0.,   5.]],
+        index=vocab, columns=vocab)
+    return df
+
+
+@pytest.mark.parametrize("arg, expected", [
+    [
+        np.array([[34.0, 11.0], [ 47.0, 7.0]]),
+        np.array([[0.92345679, 1.34444444], [1.06378601, 0.71296296]])
+    ]
+])
+def test_observed_over_expected(arg, expected):
+    result = vsm.observed_over_expected(arg).round(8)
+    assert np.array_equal(result, expected.round(8))
+
+
+@pytest.mark.parametrize("arg, expected, positive", [
+    [
+        np.array([[34.0, 11.0], [ 47.0, 7.0]]),
+        np.array([[0.0, 0.29598088], [0.06183425, 0.0]]),
+        True
+    ],
+    [
+        np.array([[34.0, 11.0], [ 47.0, 7.0]]),
+        np.array([[-0.07963127, 0.29598088], [0.06183425, -0.33832581]]),
+        False
+    ],
+    [
+        np.array([[1.0, 0.0, 0.0], [1000.0, 1000.0, 4000.0], [1000.0, 2000.0, 999.0]]),
+        np.array([[1.60893804, 0., 0.],[0., 0., 0.28788209], [0.22289371, 0.51107566, 0.]]),
+        True
+    ]
+])
+def test_pmi(arg, expected, positive):
+    result = vsm.pmi(arg, positive=positive).round(8)
+    assert np.array_equal(result, expected.round(8))
+
+
+@pytest.mark.parametrize("arg, expected", [
+    [
+        np.array([[34.0, 0.0], [0.0, 7.0]]),
+        np.array([[0.69314718, 0.0], [0.0, 0.69314718]])
+    ],
+    [
+        np.array([
+            [10.0, 10.0, 10.0, 10.0],
+            [10.0, 10.0, 10.0,  0.0],
+            [10.0, 10.0,  0.0,  0.0],
+            [0.0,   0.0,  0.0,  1.0]]),
+        np.array([
+            [0.0, 0.0, 0.0, 0.0],
+            [0.09589402, 0.09589402, 0.14384104, 0.],
+            [0.23104906, 0.23104906, 0., 0.],
+            [0., 0., 0., 0.12602676]])
+    ]
+])
+def test_tfidf(arg, expected):
+    result = vsm.tfidf(arg).round(8)
+    expected = expected.round(8)
+    assert np.array_equal(result, expected)
+
+
+@pytest.mark.parametrize("bigram, expected", [
+    [
+        '<w>a', np.array([4.0, 4.0, 2.0, 0.0])
+    ],
+    [
+        '<w>b',  np.array([4.0, 61.0, 8.0, 18.0])
+    ],
+])
+def test_ngram_vsm(df, bigram, expected):
+    X = vsm.ngram_vsm(df)
+    result = X.loc[bigram]
+    assert np.array_equal(result, expected)
+
+
+@pytest.mark.parametrize("word, n, expected", [
+    ['abc', 1, ['a', 'b', 'c']],
+    ['abc', 2, ['<w>a', 'ab', 'bc', 'c</w>']]
+])
+def test_get_character_ngrams(word, n, expected):
+    result = vsm.get_character_ngrams(word, n=n)
+    assert result == expected
+
+
+def test_tsne_viz(df):
+    vsm.tsne_viz(df)
+
+
+def test_lsa(df):
+    vsm.lsa(df, k=2)
+
+
+def test_glove(df):
+    """Just makes sure that this code will run; it doesn't check that
+    it is creating good models.
+    """
+    vsm.glove(df, n=100, xmax=100, alpha=0.75, max_iter=100, eta=0.05,
+        tol=1e-4, display_progress=True)
