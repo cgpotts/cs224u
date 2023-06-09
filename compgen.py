9c182d168831dc28f7ef802f1ac23370ae630114
Christopher Potts <cgpotts@gmail.com>
2023-06-09 08:02:50 -0700

    Improved ReCOGS scoring function

--- compgen.py
+++ compgen.py
@@ -1,189 +1,117 @@
+from collections import defaultdict
+from itertools import product
 import re
 
-def find_partition_name(name, lf):
-    if lf == "cogs":
-        return name
-    else:
-        return name+f"_{lf}"
-    
-def check_equal(left_lf, right_lf):
-    index_mapping = {}
-    current_idx = 0
-    for t in left_lf.split():
-        if t.isnumeric():
-            if int(t) not in index_mapping:
-                index_mapping[int(t)] = current_idx
-                current_idx += 1
-    decoded_labels_ii = []
-    for t in left_lf.split():
-        if t.isnumeric():
-            decoded_labels_ii += [str(index_mapping[int(t)])]
-        else:
-            decoded_labels_ii += [t]
-
-    index_mapping = {}
-    current_idx = 0
-    for t in right_lf.split():
-        if t.isnumeric():
-            if int(t) not in index_mapping:
-                index_mapping[int(t)] = current_idx
-                current_idx += 1
-    decoded_preds_ii = []
-    for t in right_lf.split():
-        if t.isnumeric():
-            decoded_preds_ii += [str(index_mapping[int(t)])]
-        else:
-            decoded_preds_ii += [t]
+__author__ = "Christopher Potts and Zhengxuan Wu"
+__version__ = "CS224u, Stanford, Spring 2023"
 
 
-    decoded_labels_ii_str = " ".join(decoded_labels_ii)
-    decoded_preds_ii_str = " ".join(decoded_preds_ii)
-
-    if decoded_preds_ii_str == decoded_labels_ii_str:
-        return True
+def recogs_exact_match(gold, pred, flag="000000"):
+    gold = normalize_formula(gold)
+    pred = normalize_formula(pred)
+    gold_conj_set = get_conj_set(gold)
+    # Loop over all viable mappings from pred_vars to gold_vars:
+    for this_map in _candidate_variable_maps(gold, pred):
+        phi = pred
+        for sourcevar, targetvar in this_map.items():
+            # The flag makes sure we don't accidentally do a chain
+            # of replacements via successive changes in situations
+            # where the domain and range of `this_map` share vars.
+            phi = variable_change(phi, sourcevar, targetvar, flag=flag)
+        phi = phi.replace(flag, "")
+        phi_conj_set = get_conj_set(phi)
+        # This step assumes that we have no conjuncts that are
+        # tautologies, contradictions, or equality predications. If
+        # such are introduced, they need to be identified ahead of
+        # time and treated separately -- tautologies would be removed,
+        # contradictions would reduce to comparisons of only those
+        # conjuncts, and equality statements would call for special
+        # handling related to variables mapping.
+        if phi_conj_set == gold_conj_set:
+            return True
     return False
 
-recogs_neoD_np_re = re.compile(r"""
-    ^
-    \s*(\*)?
-    \s*(\w+?)\s*
-    \(
-    \s*(.+?)\s*
-    \)
-    \s*$""", re.VERBOSE)
 
-recogs_neoD_verb_re = re.compile(r"""
-    ^
-    \s*(\w+?)\s*
-    \(
-    \s*([0-9]+?)\s*
-    \)
-    \s*$""", re.VERBOSE)
+def normalize_formula(phi):
+    return phi.replace(" ", "").replace("AND" , " AND ")
 
-recogs_neoD_pred_re = re.compile(r"""
-    ^
-    \s*(\w+?)\s*
+
+binary_pred_re = re.compile(r"""
+    (\w+)
+    \s*
     \(
-    \s*(.+?)\s*
+    \s*
+    (\d+)
+    \s*
     ,
-    \s*(.+?)\s*
-    \)
-    \s*$""", re.VERBOSE)
+    \s*
+    (\d+)
+    \s*
+    \)""", re.VERBOSE)
 
-recogs_neoD_mod_re = re.compile(r"""
-    ^
-    \s*(\w+?)\s*
-    \.
-    \s*(\w+?)\s*
+
+unary_pred_re = re.compile(r"""
+    (\w+)
+    \s*
     \(
-    \s*(.+?)\s*
-    ,
-    \s*(.+?)\s*
-    \)
-    \s*$""", re.VERBOSE)
+    \s*
+    (\d+)
+    \s*
+    \)""", re.VERBOSE)
 
-def translate_invariant_form_neoD(lf):
-    nouns = lf.split(" AND ")[0].split(" ; ")[:-1]
-    complements = set(lf.split(" ; ")[-1].split())
-    nouns_map = {}
-    new_var = 0
-    for noun in nouns:
-        # check format.
-        if not recogs_neoD_np_re.search(noun):
-            return {} # this is format error, we cascade the error.
-        _, _, original_var = recogs_neoD_np_re.search(noun).groups()
-        if original_var not in complements:
-            return {} # var must be used, we cascade the error.
-        new_noun = noun.replace(str(original_var), str(new_var))
-        nouns_map[original_var] = new_noun
-        new_var += 1
-        
-    nmod_conjs_set = set([])
-    conjs = lf.split(" ; ")[-1].split(" AND ")
-    vp_conjs_map = {}
-    nested_conjs = []
-    childen_count_map = {}
-    for conj in conjs:
-        if "nmod" in conj:
-            if not recogs_neoD_mod_re.search(conj):
-                return {} # this is format error, we cascade the error.
-            role, pred, first_arg, second_arg = recogs_neoD_mod_re.search(conj).groups()
-            new_conj = f"{role} . {pred} ( {nouns_map[first_arg]} , {nouns_map[second_arg]} )"
-            nmod_conjs_set.add(new_conj)
-        else:
-            if recogs_neoD_verb_re.search(conj):
-                # candidate for mapping verb.
-                pred, arg = recogs_neoD_verb_re.search(conj).groups()
-                if not arg.isnumeric():
-                    return {}
-                new_conj = f"{pred}"
-                if arg in vp_conjs_map:
-                    vp_conjs_map[arg].append(new_conj)
-                else:
-                    vp_conjs_map[arg] = [new_conj]
-                continue
-            if not recogs_neoD_pred_re.search(conj):
-                return {} # this is format error, we cascade the error.
-            
-            role, first_arg, second_arg = recogs_neoD_pred_re.search(conj).groups()
-            if first_arg == second_arg or first_arg in nouns_map or not first_arg.isnumeric():
-                return {} # this is index collision, we cascade the error.
-            if second_arg.isnumeric() and second_arg in nouns_map:
-                second_arg = nouns_map[second_arg]
-                new_conj = f"{role} ( {second_arg} )"
-                if first_arg in vp_conjs_map:
-                    vp_conjs_map[first_arg].append(new_conj)
-                else:
-                    vp_conjs_map[first_arg] = [new_conj]
-            elif second_arg.isnumeric():
-                if first_arg not in childen_count_map:
-                    childen_count_map[first_arg] = 1
-                else:
-                    childen_count_map[first_arg] += 1
-                nested_conjs.append({
-                    "role": role,
-                    "first_arg": first_arg,
-                    "second_arg": second_arg,
-                })
-            else:
-                return {}
-    
-    while_loop_count = 0
-    while len(nested_conjs) > 0:
-        while_loop_count += 1
-        if while_loop_count > 100:
-            return {}
-        conj = nested_conjs.pop(0)
-        if conj['second_arg'] not in childen_count_map or childen_count_map[conj['second_arg']] == 0:
-            core = " AND ".join(vp_conjs_map[conj['second_arg']])
-            vp_conjs_map[conj['first_arg']].append(f"{conj['role']} ( {core} )")
-            childen_count_map[conj['first_arg']] -= 1
-        else:
-            # if the conj is corrupted, then we abandon just let it go and fail to compare.
-            if conj['first_arg'] == conj['second_arg']:
-                return {}
-            nested_conjs.append(conj)
-    
-    filtered_conjs_set = set([])
-    for k, v in vp_conjs_map.items():
-        vp_conjs_map[k].sort()
-    for k, v in vp_conjs_map.items():
-        vp_expression = " AND ".join(v)
-        if vp_expression in filtered_conjs_set:
-            return {} # this is not allowed. exact same VP expression is not allowed this time.
-        filtered_conjs_set.add(vp_expression)
-    for conj in nmod_conjs_set:
-        if conj in filtered_conjs_set:
-            return {} # this is not allowed. exact same VP expression is not allowed this time.
-        filtered_conjs_set.add(conj)
-    return filtered_conjs_set
 
-def check_set_equal_neoD(left_lf, right_lf):
-    try:
-        if translate_invariant_form_neoD(left_lf) == \
-        translate_invariant_form_neoD(right_lf):
-            return True
-        else:
-            return False
-    except:
-        return False
+def _candidate_variable_maps(gold, pred):
+    # This creates a mapping from tuples of predicates into their
+    # associated variables. These serve as equivalence classes over
+    # variables that could possibly be translations of each other.
+    gold_map = _map_get_preds_to_vars(gold)
+    pred_map = _map_get_preds_to_vars(pred)
+
+    # For each prediction variable, get the set of potential
+    # translations for it:
+    pred2gold = defaultdict(list)
+    for preds, pvars in pred_map.items():
+        gvars = gold_map[preds]
+        for pvar in pvars:
+            pred2gold[pvar] = gold_map[preds]
+
+    # Variable sets:
+    gold_vars = set(get_variables(gold))
+    pred_vars = set(get_variables(pred))
+
+    # Now generate potentially viable mappings:
+    for vals in list(product(*list(pred2gold.values()))):
+        d = dict(zip(pred2gold.keys(), vals))
+        if set(d.keys()) == pred_vars and set(d.values()) == gold_vars:
+            yield d
+
+
+def _map_get_preds_to_vars(phi):
+    var2pred = defaultdict(list)
+    for pred, var in unary_pred_re.findall(phi):
+        var2pred[var].append(pred)
+    # We could do somewhat less search by specializing to first and
+    # second position for these predicates, but I think it's fine
+    # as-is.
+    for pred, var1, var2 in binary_pred_re.findall(phi):
+        var2pred[var1].append(pred)
+        var2pred[var2].append(pred)
+    pred2var = defaultdict(list)
+    for var, preds in var2pred.items():
+        pred2var[tuple(sorted(preds))].append(var)
+    return pred2var
+
+
+def get_variables(phi):
+    variable_re = re.compile(r"(\d+)")
+    return variable_re.findall(phi)
+
+
+def get_conj_set(phi):
+    conj_splitter_re  = re.compile(r"\s*(?:AND|;)\s*")
+    return set(conj_splitter_re.split(phi))
+
+
+def variable_change(phi, sourcevar, targetvar, flag="000000"):
+    replace_re = re.compile(rf"\b{sourcevar}\b")
+    return replace_re.sub(f"{flag}{targetvar}", phi)
